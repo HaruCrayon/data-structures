b0f04ecb3cb12a534dea14f9da1a5e83feb9807c
jinglige <leejing16306@163.com>
2023-04-21 16:29:27 +0800

    Binary Search

---
+++ src/com/lee/algorithm/binarysearch/BinarySearchNoRecur.java
@@ -0,0 +1,30 @@
+package com.lee.algorithm.binarysearch;
+
+/**
+ * @author LiJing
+ * @version 1.0
+ * 二分查找算法（非递归）
+ */
+public class BinarySearchNoRecur {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 8, 10, 11, 67, 100};
+        int res = binarySearch(arr, 11);
+        System.out.println("index= " + res);
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
